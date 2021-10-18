c8d69d1f5a95785ad11444f5f69594703e37f476
Pedro Fonseca <pedrinhopedro01@gmail.com>
2021-10-18 13:38:43 -0300

    criada a funcao 'ajustaTamanhoPalcoJogo()' onde se cria a mosca dinamicamente

--- game.js
+++ game.js
@@ -8,5 +8,13 @@ function ajustaTamanhoPalcoJogo() {
 
 ajustaTamanhoPalcoJogo()
 
-let positionX = Math.floor(Math.random() * width)
-let positionY = Math.floor(Math.random() * height)
+function randomPosition() {
+  let positionX = Math.floor(Math.random() * width)
+  let positionY = Math.floor(Math.random() * height)
+
+  const fly = document.createElement('img')
+  fly.src = 'img/mosca.png'
+  fly.className = 'fly-1'
+
+  document.body.appendChild(fly)
+}
