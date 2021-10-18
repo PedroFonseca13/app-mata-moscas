b340b21acab236509299a6dede630204f599c29f
Pedro Fonseca <pedrinhopedro01@gmail.com>
2021-10-18 13:50:04 -0300

    corrigido o erro quando a mosca some na tela com cordenadas negativas

--- game.js
+++ game.js
@@ -2,19 +2,27 @@ let height = 0
 let width = 0
 
 function ajustaTamanhoPalcoJogo() {
-  let height = window.innerHeight
-  let width = window.innerWidth
+  height = window.innerHeight
+  width = window.innerWidth
 }
 
 ajustaTamanhoPalcoJogo()
 
 function randomPosition() {
-  let positionX = Math.floor(Math.random() * width)
-  let positionY = Math.floor(Math.random() * height)
+  let positionX = Math.floor(Math.random() * width) - 90
+  let positionY = Math.floor(Math.random() * height) - 90
+
+  positionX = positionX < 0 ? 0 : positionX
+  positionY = positionY < 0 ? 0 : positionY
+
+  console.log(positionX, positionY);
 
   const fly = document.createElement('img')
   fly.src = 'img/mosca.png'
   fly.className = 'fly-1'
+  fly.style.left = positionX + 'px'
+  fly.style.top = positionY + 'px'
+  fly.style.position = 'absolute'
 
   document.body.appendChild(fly)
 }
